cb9f410751a9e14a7ad4866baa9ff3a09d46f684
Dave Cheli <dcheli@live.com>
2017-11-22 08:48:15 -0600

    Adding frontend files to Github Pages

--- deployfrontend.cmd
+++ deployfrontend.cmd
@@ -1,3 +1,5 @@
+Copy-Item -Recurse src\ -Destination docs\  -Verbose
+Copy-Item build\contracts\ChainList.json -Destination docs\  -Verbose
 git add .
 git commit -m "Adding frontend files to Github Pages"
 git push
